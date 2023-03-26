c76dffa02b6001f08f03810f0183ca6f59bcc754
ksnbkvais <114652308+ksnbkvais@users.noreply.github.com>
2023-03-26 19:30:55 +0600

    Create 1.py

---
+++ lab7/hackerrank/1.py
@@ -0,0 +1,16 @@
+a=int(input())
+b=int(input())
+print(a//b)
+print(a/b)
+"""
+Example
+a=3
+b=5
+
+The result of the integer division . 3:5=0
+The result of the float division is .3:5=0.6
+Print:
+
+0
+0.6
+"""
