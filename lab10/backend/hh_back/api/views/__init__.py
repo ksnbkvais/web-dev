bfbc3033693db25c53d1e808e746964696fb6d9d
ksnbkvais <114652308+ksnbkvais@users.noreply.github.com>
2023-04-23 13:21:45 +0600

    Delete lab10/backend/hh_back/api/views directory

--- lab10/backend/hh_back/api/views/__init__.py
+++
@@ -1,5 +0,0 @@
-# from .view_1 import companies_list,get_company,company_vacancy,get_vacancy,vacancy_list,top_vacancy
-# from .view_2 import companies_list,get_company,company_vacancy,get_vacancy,vacancy_list,top_vacancy
-# from .view_3 import companies_list,get_company,company_vacancy,get_vacancy,vacancy_list,top_vacancy
-# from .view_4 import CompanyListAPIView, CompanyVacanciesAPIView,VacancyItemAPIView,VacancyListAPIView,TopVacancyAPIView,CompanyItemAPIView
-from .view_5 import  CompanyListAPIView, CompanyVacanciesAPIView,VacancyItemAPIView,VacancyListAPIView,TopVacancyAPIView,CompanyItemAPIView
